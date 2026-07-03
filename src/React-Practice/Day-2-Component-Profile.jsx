ff359bc62900102df59f59821e32afe612a240b7
ShahLucky444 <shahlucky755@gmail.com>
2026-07-03 19:45:06 +0530

    learn about components and do some practical question

---
+++ src/React-Practice/Day-2-Component-Profile.jsx
@@ -0,0 +1,24 @@
+// function Profile() {
+//   return (
+//     <div>
+//       <h1>bhil ratan</h1>
+//       <p>web developer</p>
+//     </div>
+//   );
+// }
+
+// export default Profile;
+
+// question practical
+
+function Navbar() {
+  return (
+    <div className="navbar">
+      <a href="">home</a>
+      <a href="">about</a>
+      <a href="">contact</a>
+    </div>
+  );
+}
+
+export default Navbar;
---
+++ src/React-Practice/Day-2-StudentCard.jsx
@@ -0,0 +1,25 @@
+function StudentCard() {
+  return (
+    <div>
+      <h2>name:lucky</h2>
+      <h2>age:19</h2>
+      <h2>course:Bca</h2>
+    </div>
+  );
+}
+
+export default StudentCard;
+
+// import Navbar from "./React-Practice/Day-2-Component-Profile.jsx";
+// import StudentCard from "./React-Practice/Day-2-StudentCard.jsx";
+
+// function App() {
+//   return (
+//     <div>
+//       <Navbar />
+//       <StudentCard />
+//     </div>
+//   );
+// }
+
+// export default App;
