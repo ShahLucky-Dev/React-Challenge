3fe48de27fdcc774b5fe424305baa8dc0a3775c1
ShahLucky444 <shahlucky755@gmail.com>
2026-07-13 20:47:20 +0530

    map practice

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,17 @@
-function App() {}
+import { useState } from "react";
+
+function App() {
+  const fruits = ["Apple", "Banana", "Mango"];
+  return (
+    <div>
+      {fruits.map((fruit) => {
+        return (
+          <div key={fruit}>
+            <h1>{fruit}</h1>
+          </div>
+        );
+      })}
+    </div>
+  );
+}
+export default App;
