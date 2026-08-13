e318012a3ed8a9b1ef23e16e39b3c78cc3ac3926
ShahLucky444 <shahlucky755@gmail.com>
2026-08-13 17:38:39 +0530

    learn about useNavigate,NavLink

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,18 @@ import Home from "./pages/Home";
 import About from "./pages/About";
 import Contact from "./pages/Contact";
 import { Routes, Route } from "react-router-dom";
+import Navbar from "./pages/Navbar";
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/about" element={<About />} />
-      <Route path="/contact" element={<Contact />} />
-    </Routes>
+    <>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </>
   );
 }
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,12 @@
 import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 function Home() {
+  const navigate = useNavigate();
   return (
     <div>
       <h1>Home Page</h1>
       <Link to="/about">Go to About</Link>
+      <button onClick={() => navigate("/contact")}>Go to Contact</button>
     </div>
   );
 }
