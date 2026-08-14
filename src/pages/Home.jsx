9240f8620e98947e07ef1b80438fb18b9cd144d1
ShahLucky444 <shahlucky755@gmail.com>
2026-08-14 21:31:49 +0530

    update home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,7 +5,9 @@ function Home() {
   return (
     <div>
       <h1>Home Page</h1>
+      <br />
       <Link to="/about">Go to About</Link>
+      <br /> <br />
       <button onClick={() => navigate("/contact")}>Go to Contact</button>
     </div>
   );
