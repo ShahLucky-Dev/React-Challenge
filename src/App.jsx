77e28efd37d8ef85e00bf3f096d1fecb3ea7cfef
ShahLucky444 <shahlucky755@gmail.com>
2026-08-18 19:08:26 +0530

    making note app using react

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,17 @@
-import { useState } from "react";
+import useState from React;
 
 function App() {
-  const [name, setName] = useState("");
-
-  async function addUser() {
-    const response = await fetch("https://dummyjson.com/users/add", {
-      method: "POST",
-      headers: {
-        "Contact-Type": "application/json",
-      },
-      body: JSON.stringify({
-        firstName: name,
-      }),
-    });
-    const data = await response.json();
-
-    console.log(data);
-  }
+  const [title, setTitle] = useState("");
+  const [text,setText]= useState("");
   return (
     <div>
-      <input
-        value={name}
-        onChange={(e) => setName(e.target.value)}
-        placeholder="Enter name"
-      />
-
-      <button onClick={addUser}>Add User</button>
+      <h1>Note App</h1>
+      <br />
+      <input type="text" name="title" id="" />
+      <br />
+      <textarea name="text" id=""></textarea>
+      <br />
+      <button>Add</button>
     </div>
   );
 }
