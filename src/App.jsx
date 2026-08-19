a4fb5512f79491e529449e999f9698104782ee45
ShahLucky444 <shahlucky755@gmail.com>
2026-08-19 18:40:44 +0530

    add button event on title

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
-import useState from React;
+import { useState } from "react";
 
 function App() {
   const [title, setTitle] = useState("");
-  const [text,setText]= useState("");
+  const [text, setText] = useState("");
+
+  function Click(event) {
+    setTitle(event.target.value);
+  }
   return (
     <div>
       <h1>Note App</h1>
       <br />
-      <input type="text" name="title" id="" />
+      <input type="text" name="title" id="" onChange={Click} />
       <br />
       <textarea name="text" id=""></textarea>
       <br />
