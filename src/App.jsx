9b7c8cf4531f34ab20779e439ebb9e9832640669
ShahLucky444 <shahlucky755@gmail.com>
2026-08-20 18:40:35 +0530

    working on my note app

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,55 @@
 import { useState } from "react";
+import "./App.css";
 
 function App() {
   const [title, setTitle] = useState("");
   const [text, setText] = useState("");
+  const [notes, setNotes] = useState([]);
 
-  function Click(event) {
+  function titleChange(event) {
     setTitle(event.target.value);
   }
+  function textChange(event) {
+    setText(event.target.value);
+  }
+
+  function handleAdd() {
+    setNotes([
+      ...notes,
+      {
+        title: title,
+        text: text,
+      },
+    ]);
+    (setTitle(""), setText(""));
+  }
   return (
-    <div>
-      <h1>Note App</h1>
+    <>
+      <div id="block">
+        <h1>Note App</h1>
+        <br />
+        <input name="title" value={title} onChange={titleChange} />
+        <br />
+        <textarea name="text" value={text} onChange={textChange}></textarea>
+        <br />
+        <button onClick={handleAdd}>Add</button>
+      </div>
+
       <br />
-      <input type="text" name="title" id="" onChange={Click} />
+      <hr />
       <br />
-      <textarea name="text" id=""></textarea>
-      <br />
-      <button>Add</button>
-    </div>
+
+      <div id="allnotes">
+        {notes.map((item, index) => {
+          return (
+            <div key={index} id="notes">
+              <h1>{item.title}</h1>
+              <p>{item.text}</p>
+            </div>
+          );
+        })}
+      </div>
+    </>
   );
 }
 
