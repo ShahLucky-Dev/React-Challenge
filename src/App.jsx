71ecb3ae6d0eaeeb7495a1544e4590f090ca9251
ShahLucky444 <shahlucky755@gmail.com>
2026-08-28 22:19:59 +0530

    add reset & +5 button

--- src/App.jsx
+++ src/App.jsx
@@ -10,12 +10,29 @@ function App() {
   function decrease() {
     setcount(count - 1);
   }
+
+  function plusFive() {
+    setcount(count + 5);
+  }
+
+  function reset() {
+    setcount(0);
+  }
+
+  if (count === 108) {
+    setcount("mala completed");
+  }
   return (
     <div>
+      <pre> Shree Radhe</pre>
       <h1>{count}</h1>
       <button onClick={decrease}>-</button>
 
       <button onClick={increace}>+</button>
+
+      <button onClick={plusFive}>+5</button>
+
+      <button onClick={reset}>reset</button>
     </div>
   );
 }
