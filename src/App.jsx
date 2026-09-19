8bd0d5ec641db6790a09894e954e53880a4db94d
ShahLucky444 <shahlucky755@gmail.com>
2026-09-19 20:27:56 +0530

    practice filter

--- src/App.jsx
+++ src/App.jsx
@@ -11,18 +11,43 @@
 
 // export default App;
 
+// function App() {
+//   const users = [
+//     { id: 1, name: "Lucky", age: 20 },
+//     { id: 2, name: "Rahul", age: 21 },
+//     { id: 3, name: "Amit", age: 19 },
+//   ];
+//   return (
+//     <div>
+//       {users.map((user) => {
+//         return (
+//           <p key={user.id}>
+//             {user.name}-{user.age}
+//           </p>
+//         );
+//       })}
+//     </div>
+//   );
+// }
+
+// export default App;
+
 function App() {
-  const users = [
-    { id: 1, name: "Lucky", age: 20 },
-    { id: 2, name: "Rahul", age: 21 },
-    { id: 3, name: "Amit", age: 19 },
+  const products = [
+    { id: 1, name: "Laptop", price: 50000 },
+    { id: 2, name: "Mouse", price: 500 },
+    { id: 3, name: "Keyboard", price: 1500 },
+    { id: 4, name: "Monitor", price: 12000 },
   ];
+
+  const productFilter = products.filter((product) => product.price > 11000);
+
   return (
     <div>
-      {users.map((user) => {
+      {productFilter.map((product) => {
         return (
-          <p key={user.id}>
-            {user.name}-{user.age}
+          <p key={product.id}>
+            {product.name} - ₹{product.price}
           </p>
         );
       })}
