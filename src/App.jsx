59465d33f5590d98a3b91ca9fddea5d469a9a885
ShahLucky444 <shahlucky755@gmail.com>
2026-09-20 19:34:49 +0530

    practice map

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,16 @@
 function App() {
-  const students = [
-    { id: 1, name: "Lucky", marks: 85 },
-    { id: 2, name: "Rahul", marks: 42 },
-    { id: 3, name: "Amit", marks: 76 },
-    { id: 4, name: "Jay", marks: 35 },
+  const users = [
+    { id: 1, name: "lucky", age: 20 },
+    { id: 2, name: "ratan", age: 20 },
+    { id: 3, name: "shivlal", age: 19 },
   ];
 
-  const filterStudent = students.filter((mark) => mark.marks >= 50);
   return (
     <div>
-      {filterStudent.map((mark) => {
+      {users.map((user, id) => {
         return (
-          <p key={mark.id}>
-            {mark.name}-{mark.marks}
+          <p key={user.id}>
+            {user.name}-{user.age}
           </p>
         );
       })}
