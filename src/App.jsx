e607e8131cd19912d01995eb9308f53cd9c6bde6
ShahLucky444 <shahlucky755@gmail.com>
2026-09-22 20:21:57 +0530

    learn about find ()

--- src/App.jsx
+++ src/App.jsx
@@ -4,31 +4,4 @@ function App() {
     { id: 2, name: "Ratan", marks: 42 },
     { id: 3, name: "Shivlal", marks: 76 },
   ];
-
-  const edit = 2;
-
-  const updatedStudends = students.map((student) => {
-    if (student.id === edit) {
-      marks: 60;
-      return {
-        ...student,
-        marks: 60,
-      };
-    }
-    return student;
-  });
-
-  return (
-    <div>
-      {updatedStudends.map((student) => {
-        return (
-          <p key={student.id}>
-            {student.name}-{student.marks}
-          </p>
-        );
-      })}
-    </div>
-  );
 }
-
-export default App;
