bd357b6b7dea04950827586d3d74e40f93a1b88c
ShahLucky444 <shahlucky755@gmail.com>
2026-09-26 10:49:03 +0530

    execute add new student using state

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { useState } from "react";
-
 function App() {
   const [students, setStudents] = useState([
     { id: 1, name: "Lucky", age: 20 },
     { id: 2, name: "Ratan", age: 21 },
-    { id: 3, name: "Shivlal", age: 22 },
   ]);
+
+  function handleAdd() {
+    setStudents([...students, { id: 3, name: "Shivlal", age: 22 }]);
+  }
 
   return (
     <div>
@@ -16,6 +18,7 @@ function App() {
           </p>
         );
       })}
+      <button onClick={handleAdd}>Add Student</button>
     </div>
   );
 }
