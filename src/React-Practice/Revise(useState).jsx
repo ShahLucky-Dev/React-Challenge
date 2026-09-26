17974260b840c929401948f7e8a1609c7a3a509e
ShahLucky444 <shahlucky755@gmail.com>
2026-09-26 20:04:42 +0530

    practice about crud

--- src/React-Practice/Revise(useState).jsx
+++ src/React-Practice/Revise(useState).jsx
@@ -108,3 +108,32 @@ function App() {
 }
 
 export default App;*/
+
+/*import { useState } from "react";
+function App() {
+  const [students, setStudents] = useState([
+    { id: 1, name: "Lucky", age: 20 },
+    { id: 2, name: "Ratan", age: 21 },
+    { id: 3, name: "Shivlal", age: 22 },
+  ]);
+
+  function handleDelete(id) {
+    const newArray = students.filter((student) => student.id !== id);
+    setStudents(newArray);
+  }
+
+  return (
+    <div>
+      {students.map((student) => {
+        return (
+          <p key={student.id}>
+            {student.name}-{student.age}
+            <button onClick={() => handleDelete(student.id)}>Delete</button>
+          </p>
+        );
+      })}
+    </div>
+  );
+}
+
+export default App;*/
